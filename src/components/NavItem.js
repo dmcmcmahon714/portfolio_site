60e0cf07a239995d47edf6686dd521488248c2ca
dmcmcmahon714 <57333568+dmcmcmahon714@users.noreply.github.com>
2020-06-01 09:44:21 -0500

    Starting to wire up app.js

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -3,13 +3,11 @@ import { Link } from "react-router-dom";
 
 class Navitem extends Component {
 
-render() {
-
+	render() {
 	return (
-		<li>
-			<Link to={this.props.tolink} >
-			{this.props.item}
-		</Link>
+
+		<li id={this.props.item}>
+		<Link to={this.props.tolink} onClick={this.props.activec.bind(this,this.props.item)}>{this.props.item}</Link>
 		</li>
 		)
 	}
