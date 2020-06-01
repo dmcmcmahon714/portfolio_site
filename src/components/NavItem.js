0465c793685a3fd33c6caf25479ae257d9bbc4e8
dmcmcmahon714 <57333568+dmcmcmahon714@users.noreply.github.com>
2020-06-01 09:31:19 -0500

    added navbar and navitem components, some styling

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -1,1 +1,17 @@
-NavItem.js
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
+class Navitem extends Component {
+
+render() {
+
+	return (
+		<li>
+			<Link to={this.props.tolink} >
+			{this.props.item}
+		</Link>
+		</li>
+		)
+	}
+}
+export default Navitem
