f1e77ae91b0ec0143326739ee84b3c90b0dea733
dmcmcmahon714 <57333568+dmcmcmahon714@users.noreply.github.com>
2020-06-01 10:05:34 -0500

    added social components

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,1 +1,13 @@
-Social.js
+import React, { Component } from 'react';
+class Social extends Component {
+
+	render() {
+		return (
+			<div class="social">
+				<a href="https://github.com/dmcmcmahon714" target="_blank"><i class="fab fa-github"></i></a>
+				<a href="https://www.linkedin.com/in/danmcmahontech" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+</div>
+)
+}
+}
+export default Social
--- src/contents/Home.js
+++ src/contents/Home.js
@@ -1,1 +1,25 @@
-Home.js
+import React, { Component } from 'react';
+//importing the picture as "profilepic".
+import profilepic from '../img/profile_pic.jpg';
+import ReactTypingEffect from 'react-typing-effect';
+import Social from '../components/Social'
+
+
+class Home extends Component {
+
+
+	render() {
+		return (
+
+			<div className="condiv home">
+
+				<img src={profilepic} className="profilepic"></img>
+
+				<ReactTypingEffect className="typingeffect" text={['I'm Dan McMahon,'Welcome to my development portfolio']} speed={100} eraseDelay={700}/>
+
+				<Social />
+		</div>
+		)
+	}
+}
+export default Home
