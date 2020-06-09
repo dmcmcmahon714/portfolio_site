322e0e5e3c1fc21a4ce655a4db3a8c7da5ef7262
dmcmcmahon714 <57333568+dmcmcmahon714@users.noreply.github.com>
2020-06-08 21:10:21 -0500

    all elements in place. just need spacing

--- src/contents/Main.js
+++ src/contents/Main.js
@@ -19,6 +19,7 @@ import githublogo from '../img/githublogo.png'
 import linkedinlogo from '../img/linkedin.png'
 import herokudemoicon from '../img/herokudemoicon.png'
 import githubsmall from '../img/githubsmall.png'
+import githubpagesicon from '../img/githubpagesicon.jpeg'
 import Social from '../components/Social'
 import Aside from '../components/Aside'
 import Section from "../components/Section";
@@ -53,6 +54,7 @@ class Main extends Component {
             datestwo="September 2009 - January 2011"
             backtotop=<img src={backtotop} className="nav-logo" alt="Logo" onClick={this.scrollToTop}/>
             />
+
         <Section
             title=<h1 className="title2">Skills</h1>
             dark={true}
@@ -78,6 +80,7 @@ class Main extends Component {
             schooltwo=""
             datestwo=""
             />
+
         <Section
             title=<h1 className="title3">Contact</h1>
             subtitle=<a className="email" href="danielrichardmcmahon@gmail.com">email: danielrichardmcmahon@gmail.com</a>
@@ -88,6 +91,7 @@ class Main extends Component {
             id="contact"
             backtotop=<img src={backtotop} className="nav-logo" alt="Logo" onClick={this.scrollToTop}/>
             />
+
         <Section
             title=<h1 className="title3">absoluteJAMS</h1>
             subtitle=<div className="sitedescription">
@@ -167,7 +171,7 @@ class Main extends Component {
 
 
         <Section
-          title=<h1 className="title3">Your New Music</h1>
+          title=<h1 className="title3">Your Release Tracker</h1>
           subtitle=<div className="sitedescription">
           <div className="column">This app allows a user to keep track of when their favorite albums will be coming out. The API was built with Ruby on Rails and PostgresQL. Fake data provided by faker.  The back end connects to the client side with rack-cors. The front end was built in React</div>
           <div className="column">
@@ -195,7 +199,7 @@ class Main extends Component {
             </div>
           dark={true}
           id="yournewmusic"
-          screenshot=<img src={YournewmusicSS} alt="Gigglelibs"/>
+          screenshot=<img src={YournewmusicSS} alt="YournewmusicSS"/>
           link=<div className="demolinks"><a href="https://frozen-temple-96234.herokuapp.com/">
           <img alt="herokudemoicon" src={herokudemoicon}/></a><a href="https://github.com/dmcmcmahon714/project_4_front_end">Front End:
           <img alt="githubsmall" src={githubsmall}/>
@@ -208,23 +212,69 @@ class Main extends Component {
 
 
         <Section
-          title="Daily Pain Tracker"
-          subtitle="This is an app designed to allow users to easily record how a specific pain or ailment is progressing over time.  Ideally this would help doctors looking for more accurate self-reporting.  This is an MVC format app with full CRUD functinality.  The database was created with MongoDB.  Authentication was created with bcrypt and express-session to allow users to sign up, sign in, and log out, with sole access to their private data. "
+          title=<h1 className="title3">Daily Pain Tracker</h1>
+          subtitle=<div className="sitedescription">
+          <div className="column">This is an app designed to allow users to easily record how a specific pain or ailment is progressing over time.  Ideally this would help doctors looking for more accurate self-reporting.  This is an MVC format app with full CRUD functinality.  The database was created with MongoDB.  Authentication was created with bcrypt and express-session to allow users to sign up, sign in, and log out, with sole access to their private data.</div>
+          <div className ="column">
+          <p className="features">Features:</p>
+           <ul>
+            <li className="descriptionitems">User Authentication</li>
+            <li className="descriptionitems">Private Pain Level Tracker</li>
+            <li className="descriptionitems">Date and Time Form Inputs</li>
+            <li className="descriptionitems">Large Text and Features for Accessibility</li>
+            </ul>
+            </div>
+            <div className="column">
+            <p className="features">Technoligies Used:</p>
+            <ul>
+            <li className="descriptionitems">Node.js</li>
+            <li className="descriptionitems">EJS</li>
+            <li className="descriptionitems">Express</li>
+            <li className="descriptionitems">Express-Session</li>
+            <li className="descriptionitems">Mongoose</li>   
+            </ul>
+            </div>
+            </div>
           dark={true}
           id="dailypaintracker"
-          repo=<a href="https://github.com/dmcmcmahon714/seir-projects2">Github Repository</a>
-          screenshot=<img src={PaintrackerSS} alt="Gigglelibs" width="900" height="800"/>
-          link=<a href="https://fierce-scrubland-02207.herokuapp.com/">Demo</a>
+          screenshot=<img src={PaintrackerSS}/>
+          link=<div className="demolinks"><a href="https://fierce-scrubland-02207.herokuapp.com//">
+            <img alt="herokudemoicon" src={herokudemoicon}/></a>
+            <a href="https://github.com/dmcmcmahon714/seir-projects2">
+            <img alt="githubsmall" src={githubsmall}/></a></div>
           backtotop=<img src={backtotop} className="nav-logo" alt="Logo" onClick={this.scrollToTop}/>
         />
+
         <Section
-          title="Chicago Tow Find"
-          subtitle="Chicago tow find is an html website that allows users to see if their car has been towed in Chicago on a given date.  The app is built with simple html, css, and javascript utilizing jquery.  The information is pulled from the official City of Chicago public API via AJAX request."
+          title=<h1 className="title3">Chicago Tow Find</h1>
+          subtitle=<div className="sitedescription">
+          <div className="column">Chicago tow find is an html website that allows users to see if their car has been towed in Chicago on a given date.  The app is built with simple html, css, and javascript utilizing jquery.  The information is pulled from the official City of Chicago public API via AJAX request.</div>
+          <div className="column">
+          <p className="features">Features:</p>
+           <ul>
+            <li className="descriptionitems">Access to Chicago Public Database</li>
+            <li className="descriptionitems">Find Vehicle by Plate, Make, and Style</li>
+            <li className="descriptionitems">Current for Past 60 days</li>
+            </ul>
+            </div>
+            <div className="column">
+            <p className="features">Technoligies Used:</p>
+            <ul>
+            <li className="descriptionitems">HTML</li>
+            <li className="descriptionitems">Javascript</li>
+            <li className="descriptionitems">Jquery</li>
+            <li className="descriptionitems">Ajax</li>
+            <li className="descriptionitems">External API</li>   
+            </ul>
+            </div>
+            </div>
           dark={true}
           id="chicagotowfind"
-          repo=<a href="https://github.com/dmcmcmahon714/Project-1">Github Repository</a>
-          screenshot=<img src={ChicagotowfindSS} alt="Gigglelibs" width="900" height="1250"/>
-          link=<a href="https://dmcmcmahon714.github.io/Project-1/">Demo</a>
+          screenshot=<img src={ChicagotowfindSS} alt="chitowfind"/>
+          link=<div className="demolinks"><a href="https://dmcmcmahon714.github.io/Project-1//">
+            <img alt="githubpagesicon" src={githubpagesicon}/></a>
+            <a href="https://github.com/dmcmcmahon714/Project-1">
+            <img alt="githubsmall" src={githubsmall}/></a></div>
           backtotop=<img src={backtotop} className="nav-logo" alt="Logo" onClick={this.scrollToTop}/>
         />
       </main>
